379a0b878908775f3ee0879c8b261d743afb49e8
SamirJ1134 <98416598+SamirJ1134@users.noreply.github.com>
2022-04-06 21:42:00 -0400

    Add files via upload

---
+++ P1SpringBootApp/src/test/java/com/revproject1/test/SpringBootJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revproject1.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
