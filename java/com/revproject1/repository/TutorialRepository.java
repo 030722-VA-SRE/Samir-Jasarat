1f3103155d3f33bcd30c1a4107123a7661710468
SamirJ1134 <98416598+SamirJ1134@users.noreply.github.com>
2022-04-08 09:53:10 -0400

    Final Project 1 commit

---
+++ java/com/revproject1/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.revproject1.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.revproject1.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
